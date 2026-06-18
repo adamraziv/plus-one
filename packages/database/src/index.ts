f49b4f254a1dfea646711d118feb6ad5fd00928a
adamraziv <adamraziv@gmail.com>
2026-06-18 16:19:21 +0800

    fix: export mutation repositories

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -3,3 +3,5 @@ export * from './errors.js';
 export * from './migrations/runner.js';
 export * from './pools.js';
 export * from './repositories/operations/index.js';
+export { PostgresDomainCommandBridge } from './repositories/operations/domain-command-bridge.js';
+export { PostgresMutationCommandRepository } from './repositories/operations/mutation-command-repository.js';
