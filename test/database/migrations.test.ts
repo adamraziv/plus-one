da0732fcde66c068e2db61f1721ef3371a8c24f1
adamraziv <adamraziv@gmail.com>
2026-06-30 22:51:54 +0800

    feat: add active channel conversation lanes

--- test/database/migrations.test.ts
+++ test/database/migrations.test.ts
@@ -40,6 +40,7 @@ describe('platform migrations', () => {
       '0014_mastra_workflow_storage.sql',
       '0015_category_spend_monthly.sql',
       '0016_mastra_observational_memory_storage.sql',
+      '0017_channel_conversation_active_lanes.sql',
     ]);
     expect(await runMigrations(options)).toEqual([]);
     await expect(verifyMigrations(options)).resolves.toBeUndefined();
